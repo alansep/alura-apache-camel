b21b634235ad461403222468fb4b266d970674a4
Gabriel Alan <alang.von@gmail.com>
2020-09-28 15:05:27 -0300

    Adequação para último estado
    Adequação para o último estado do código no início da Aula 3.

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,8 +14,10 @@ public class RotaPedidos {
             @Override
             public void configure() throws Exception {
                 from("file:pedidos?delay=5s&noop=true")
-                        .filter().xpath()
-                        .log("${id} - ${body}")
+                        .log("${id}")
+                        .marshal().xmljson()
+                        .log("${body}")
+                        .setHeader("CamelFileName", simple("${file:name.noext}.json"))
                         .to("file:saida");
             }
         });
